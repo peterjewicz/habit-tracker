1bbefce659a2d8480410e75781940b51f70b1818
peterjewicz <peterjewicz@totalwebconnections.com>
2018-11-22 12:48:18 -0600

    Fix for scrolling on devices

--- mobile/www/js/compiled/out/habit_tracker/components/Dashboard.cljs
+++ mobile/www/js/compiled/out/habit_tracker/components/Dashboard.cljs
@@ -2,14 +2,19 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [habit_tracker.components.Habit :as Habit]
             [habit_tracker.utils.view_handler :as view_handler]
-            [habit_tracker.components.New :as New]))
+            [habit_tracker.components.New :as New]
+            [habit_tracker.components.Tutorial :as Tutorial]))
 
 (defn render []
   (fn []
     (let [currentStorage (js->clj (.parse js/JSON (.getItem (.-localStorage js/window) "habits")))
-          habits (atom currentStorage)]
+          habits (atom currentStorage)
+          firstTime (js->clj (.parse js/JSON (.getItem (.-localStorage js/window) "firstTime")))]
+      (if (not firstTime)
+        (view_handler/tutorial-view-active))
       [:div.Dashboard
         (New/render view_handler/active-view habits)
+        (Tutorial/render view_handler/active-view)
         [:div.Header
         [:h3.title.uppercase "Your Habits"]]
         [:div.Dashboard-content
--- mobile/www/js/compiled/out/habit_tracker/components/Dashboard.js
+++ mobile/www/js/compiled/out/habit_tracker/components/Dashboard.js
@@ -5,31 +5,38 @@ goog.require('reagent.core');
 goog.require('habit_tracker.components.Habit');
 goog.require('habit_tracker.utils.view_handler');
 goog.require('habit_tracker.components.New');
+goog.require('habit_tracker.components.Tutorial');
 habit_tracker.components.Dashboard.render = (function habit_tracker$components$Dashboard$render(){
 return (function (){
 var currentStorage = cljs.core.js__GT_clj.call(null,JSON.parse(window.localStorage.getItem("habits")));
 var habits = reagent.core.atom.call(null,currentStorage);
-return new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div.Dashboard","div.Dashboard",1804914400),habit_tracker.components.New.render.call(null,habit_tracker.utils.view_handler.active_view,habits),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div.Header","div.Header",-753829133),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h3.title.uppercase","h3.title.uppercase",1537702661),"Your Habits"], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div.Dashboard-content","div.Dashboard-content",-14814664),cljs.core.doall.call(null,(function (){var iter__4292__auto__ = ((function (currentStorage,habits){
-return (function habit_tracker$components$Dashboard$render_$_iter__27511(s__27512){
-return (new cljs.core.LazySeq(null,((function (currentStorage,habits){
-return (function (){
-var s__27512__$1 = s__27512;
-while(true){
-var temp__5457__auto__ = cljs.core.seq.call(null,s__27512__$1);
-if(temp__5457__auto__){
-var s__27512__$2 = temp__5457__auto__;
-if(cljs.core.chunked_seq_QMARK_.call(null,s__27512__$2)){
-var c__4290__auto__ = cljs.core.chunk_first.call(null,s__27512__$2);
-var size__4291__auto__ = cljs.core.count.call(null,c__4290__auto__);
-var b__27514 = cljs.core.chunk_buffer.call(null,size__4291__auto__);
-if((function (){var i__27513 = (0);
-while(true){
-if((i__27513 < size__4291__auto__)){
-var habit = cljs.core._nth.call(null,c__4290__auto__,i__27513);
-cljs.core.chunk_append.call(null,b__27514,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [habit_tracker.components.Habit.render,habit], null));
+var firstTime = cljs.core.js__GT_clj.call(null,JSON.parse(window.localStorage.getItem("firstTime")));
+if(cljs.core.not.call(null,firstTime)){
+habit_tracker.utils.view_handler.tutorial_view_active.call(null);
+} else {
+}
 
-var G__27515 = (i__27513 + (1));
-i__27513 = G__27515;
+return new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div.Dashboard","div.Dashboard",1804914400),habit_tracker.components.New.render.call(null,habit_tracker.utils.view_handler.active_view,habits),habit_tracker.components.Tutorial.render.call(null,habit_tracker.utils.view_handler.active_view),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div.Header","div.Header",-753829133),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h3.title.uppercase","h3.title.uppercase",1537702661),"Your Habits"], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div.Dashboard-content","div.Dashboard-content",-14814664),cljs.core.doall.call(null,(function (){var iter__4292__auto__ = ((function (currentStorage,habits,firstTime){
+return (function habit_tracker$components$Dashboard$render_$_iter__27506(s__27507){
+return (new cljs.core.LazySeq(null,((function (currentStorage,habits,firstTime){
+return (function (){
+var s__27507__$1 = s__27507;
+while(true){
+var temp__5457__auto__ = cljs.core.seq.call(null,s__27507__$1);
+if(temp__5457__auto__){
+var s__27507__$2 = temp__5457__auto__;
+if(cljs.core.chunked_seq_QMARK_.call(null,s__27507__$2)){
+var c__4290__auto__ = cljs.core.chunk_first.call(null,s__27507__$2);
+var size__4291__auto__ = cljs.core.count.call(null,c__4290__auto__);
+var b__27509 = cljs.core.chunk_buffer.call(null,size__4291__auto__);
+if((function (){var i__27508 = (0);
+while(true){
+if((i__27508 < size__4291__auto__)){
+var habit = cljs.core._nth.call(null,c__4290__auto__,i__27508);
+cljs.core.chunk_append.call(null,b__27509,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [habit_tracker.components.Habit.render,habit], null));
+
+var G__27510 = (i__27508 + (1));
+i__27508 = G__27510;
 continue;
 } else {
 return true;
@@ -37,30 +44,30 @@ return true;
 break;
 }
 })()){
-return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__27514),habit_tracker$components$Dashboard$render_$_iter__27511.call(null,cljs.core.chunk_rest.call(null,s__27512__$2)));
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__27509),habit_tracker$components$Dashboard$render_$_iter__27506.call(null,cljs.core.chunk_rest.call(null,s__27507__$2)));
 } else {
-return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__27514),null);
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__27509),null);
 }
 } else {
-var habit = cljs.core.first.call(null,s__27512__$2);
-return cljs.core.cons.call(null,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [habit_tracker.components.Habit.render,habit], null),habit_tracker$components$Dashboard$render_$_iter__27511.call(null,cljs.core.rest.call(null,s__27512__$2)));
+var habit = cljs.core.first.call(null,s__27507__$2);
+return cljs.core.cons.call(null,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [habit_tracker.components.Habit.render,habit], null),habit_tracker$components$Dashboard$render_$_iter__27506.call(null,cljs.core.rest.call(null,s__27507__$2)));
 }
 } else {
 return null;
 }
 break;
 }
-});})(currentStorage,habits))
+});})(currentStorage,habits,firstTime))
 ,null,null));
-});})(currentStorage,habits))
+});})(currentStorage,habits,firstTime))
 ;
 return iter__4292__auto__.call(null,cljs.core.deref.call(null,habits));
-})()),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"p.addNewButton","p.addNewButton",-189000126),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"on-click","on-click",1632826543),((function (currentStorage,habits){
+})()),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"p.addNewButton","p.addNewButton",-189000126),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"on-click","on-click",1632826543),((function (currentStorage,habits,firstTime){
 return (function (){
 return habit_tracker.utils.view_handler.new_view_active.call(null);
-});})(currentStorage,habits))
+});})(currentStorage,habits,firstTime))
 ], null),"+"], null)], null)], null);
 });
 });
 
-//# sourceMappingURL=Dashboard.js.map?rel=1542637715435
+//# sourceMappingURL=Dashboard.js.map?rel=1542817780629
